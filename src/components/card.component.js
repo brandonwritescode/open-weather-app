1cb7201c97db47d0a624a72c0ecf6e033719ee3c
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-05-06 13:03:31 -0300

    cleanup of component css

--- src/components/card.component.js
+++ src/components/card.component.js
@@ -8,22 +8,23 @@ function WeatherCard() {
 
     return (
         <div className="card">
-            {/* <div className="bg"></div> */}
             <div className="container">
-            <form className="example" action="submit">
-                <input type="text" placeholder="Search.." name="search"></input>
-                <button type="submit"> <i className="fa fa-search"></i></button>
-            </form>  
-            <div className="weatherInfo">
-                <div className="leftCard">
-                    <h1>Sunny</h1>
-                    <h2>31 &#8451;</h2>
-                    <p>London, United Kingdom</p>
+                <div className="searchBar">
+                    <form className="example" action="submit">
+                        <input type="text" placeholder="Search.." name="search"></input>
+                        <button type="submit"> <i className="fa fa-search"></i></button>
+                    </form>  
                 </div>
-                <div className="rightCard">
-                    <img className="sunPng" src="https://img.icons8.com/android/480/000000/sun.png" alt="sun png"/>
+                <div className="weatherInfo">
+                    <div className="leftCard">
+                        <h1>Sunny</h1>
+                        <h2>31 &#8451;</h2>
+                        <p>London, United Kingdom</p>
+                    </div>
+                    <div className="rightCard">
+                        <img className="sunPng" src="https://img.icons8.com/android/480/000000/sun.png" alt="sun png"/>
+                    </div>
                 </div>
-            </div>
             </div>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
         </div>
