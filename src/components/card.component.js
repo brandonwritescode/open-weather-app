03f5906e1e0b47fc205b0fd93b152b9f33632a39
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2020-05-06 13:59:24 -0300

    adding fetch method for data retrieval

--- src/components/card.component.js
+++ src/components/card.component.js
@@ -1,9 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import RainSvg from '../assets/img/310148(2).png';
 import '../styles/card.css';
 
 
 function WeatherCard() {
 
+    const [currentWeather, setCurrentWeather] = useState({});
+    const [currentLocation, setCurrentLocation] = useState({});
+    const [validData, setValidData] = useState({});
+    const [isSunny, setIsSunny] = useState(true);
+    const [weatherSvg, setWeatherSvg] = useState('');
+
+    
+    useEffect(() => {
+        fetch("/data/2.5/weather?q=London,uk&appid=439d4b804bc8187953eb36d2a8c26a02")
+        .then(res => res.json())
+        .then(j => setCurrentWeather(j))
+        .catch(err => console.log(err))
+    }, []);
+
+    useEffect(() => {
+        
+        const condition = currentWeather.weather && currentWeather.weather[0].main;
+        const temp = currentWeather.main && parseInt(currentWeather.main.temp - 273);
+        const location = currentWeather.name;
+
+        setValidData(
+            {
+                "condition": condition,
+                "temp":temp,
+                "location": location
+            }
+            );
+
+        if(condition === "Drizzle") {
+            setIsSunny(true);
+        } else {
+
+            setIsSunny(false);
+        }
+
+
+    }, [currentWeather]);
+
+
+    useEffect(() => {
+
+        if(isSunny) {
+            console.log("IT IS SUNNY");
+            setWeatherSvg("https://img.icons8.com/android/480/000000/sun.png");
+        } else {
+            setWeatherSvg(RainSvg);
+        }
+    }, [isSunny]);
 
 
     return (
@@ -17,12 +66,12 @@ function WeatherCard() {
                 </div>
                 <div className="weatherInfo">
                     <div className="leftCard">
-                        <h1>Sunny</h1>
-                        <h2>31 &#8451;</h2>
-                        <p>London, United Kingdom</p>
+                        <h1>{validData.condition}</h1>
+                        <h2>{validData.temp}&#8451;</h2>
+                        <p>{validData.location}</p>
                     </div>
                     <div className="rightCard">
-                        <img className="sunPng" src="https://img.icons8.com/android/480/000000/sun.png" alt="sun png"/>
+                        <img className="sunPng" src={weatherSvg} alt="sun png"/>
                     </div>
                 </div>
             </div>
